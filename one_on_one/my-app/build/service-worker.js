b58af2e3e7ce6457e9e022e72e783b1606195cc1
achiu <achiu@github.shuttercorp.net>
2019-08-26 14:23:49 -0700

    push new files

--- one_on_one/my-app/build/service-worker.js
+++ one_on_one/my-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a638f12bdfbb3f3c8e72eec4112208e.js"
+  "/test/precache-manifest.3b1a2edd8703752f950b1a1468f906a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
