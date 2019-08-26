0fdaea0c3c8b722fea45e8229705420c121e7fa7
achiu <achiu@github.shuttercorp.net>
2019-08-26 14:28:15 -0700

    Updates

---
+++ precache-manifest.3b1a2edd8703752f950b1a1468f906a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb052e756ab7617516049ab59989f5b",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f6d86eb5a1cfb38c79df",
+    "url": "/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/test/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "f6d86eb5a1cfb38c79df",
+    "url": "/test/static/js/main.ce617786.chunk.js"
+  },
+  {
+    "revision": "64213a4de07e3e41dcac",
+    "url": "/test/static/js/runtime~main.6af285d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
